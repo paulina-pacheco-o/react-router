7cb9bbc796d933d094740e4fb91bdaded6017c8b
paulina-pacheco-o <paulimichi05@gmail.com>
2025-09-09 09:03:44 +0200

    products cards

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -7,15 +7,29 @@ const ProductsPage = () => {
 
   useEffect(() => {
     axios.get("https://fakestoreapi.com/products").then((resp) => {
-      console.log(resp.data);
+      setProducts(resp.data);
     });
   }, []);
   return (
     <div className="contariner">
       <div className="row">
         <div className="col-12">
-          <h1>Products</h1>
+          <h1>Pagina prodotti</h1>
         </div>
+        {products.map(product => {
+          return (
+            <div className="col-4" key={product.id}>
+              <div className="card">
+                {product.title}
+              </div>
+              <div className="card-body">
+                <p>{product.category}</p>
+                <p><em>{product.price}</em></p>
+                <p>{product.description}</p>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
